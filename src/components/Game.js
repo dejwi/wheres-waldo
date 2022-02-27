1afc2b345668144ec6ab11fa7302046c0be10678
dejwi <misti.krasula@gmail.com>
2022-02-28 00:50:26 +0100

    fixed relative positioning

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,27 +1,62 @@
 import styled from "styled-components";
+import {useState, useEffect} from "react";
+import {firestore} from "./firebase";
+import { useParams } from "react-router-dom";
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 
-const Container = styled.div`
-      position: relative;
-`;
+function Game(){
+    const params = useParams();
+    const [data, setData] = useState({});
 
-// props - data:leveldata
-function Game(props){
-    const data = props.data;
+    useEffect(() => {
+        const query = firestore.collection('waldos')
+            .where('name','==',params.level);
+        query.get().then(snap => snap.docs[0].data()).then(dat => setData(dat));
+    },[]);
+
+    const [time,setTime] = useState(0.00);
+    let inv;
+    useEffect(() => {
+        inv = setInterval(()=>{
+            setTime( Math.round((parseFloat('0.1')
+                + parseFloat(time))*100)/100);
+        },100);
+
+        return ()=> {
+            clearInterval(inv);
+        };
+    });
 
     const logClick = (e) =>{
-        const pos = {x: e.nativeEvent.layerX, y: e.nativeEvent.layerY};
+        // postion relative to the element
+        const rect = e.target.getBoundingClientRect();
+        const pos = {x: e.clientX - rect.left, y: e.clientY - rect.top};
+
         if (pos.x >= data.posX && pos.x <= data.posX + data.width){
             if (pos.y >= data.posY && pos.y <= data.posY + data.height){
                 console.log('found');
+                clearInterval(inv);
             }
         }
     };
-
+    const addLeaderboard = () => {
+        const messagesRef = firestore.collection('leaderboard')
+            .doc('beach').collection('data');
+        messagesRef.add({
+            time: time,
+            uid: 'gdfgSEFfes',
+            name: 'bruh'
+        });
+    };
     return (<div>
         <h1>Find Waldo</h1>
-        <Container>
+        <span>Time: {time}</span>
+
+        <div>
             <img src={data.imageUrl} alt='find waldo' onClick={logClick}/>
-        </Container>
+            <button onClick={addLeaderboard} >Add to leaderboard</button>
+        </div>
+
 
     </div>);
 }
