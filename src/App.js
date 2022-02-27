6d57961df4acac63c3141232cbe07ae801a4a122
dejwi <misti.krasula@gmail.com>
2022-02-27 16:49:07 +0100

    base & testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import './styles/main.scss';
+import Game from './components/Game';
+
+import firebase_config from './firebase-config.json';
+firebase.initializeApp(firebase_config);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+    const [data, setData] = useState([]);
+    useEffect(() =>{
+        firestore.collection('waldos').get().then(snapshot => {
+            setData( snapshot.docs.map(e =>
+                ({...e.data(), id: e.id}) ));
+        });
+    },[]);
+
   return (
     <div>
+        {data[0] ? <Game data={data[0]}/> : null}
     </div>
   );
 }
