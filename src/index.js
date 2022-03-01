19f26c84c2b41ea14ddec094e4b45c452aa4d3dd
dejwi <misti.krasula@gmail.com>
2022-03-01 21:46:30 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 
+
 import App from './App';
 import Game from './components/Game';
 import Leaderboard from "./components/Leaderboard";
@@ -11,7 +12,7 @@ ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/:level" element={<Game />} />
+            <Route path="/play/:level" element={<Game />} />
             <Route path="/leaderboard/:level" element={<Leaderboard />} />
         </Routes>
     </BrowserRouter>,
